b1e5d064314ad2561a39f5997a3ee39a62e8ef1f
XanderXu <517807635@qq.com>
2020-11-14 21:52:34 +0800

    fix:z

--- Distance/Distance/Line.swift
+++ Distance/Distance/Line.swift
@@ -22,7 +22,7 @@ func distanceToLine(from point:simd_float3, to line:Line) -> Float {
     let tarPoint = line.position + dotValue * normalizedDirection
     
     let disVector = point - tarPoint
-    if abs(disVector.x) < Float.leastNonzeroMagnitude, abs(disVector.y) < Float.leastNonzeroMagnitude  {
+    if abs(disVector.x) < Float.leastNonzeroMagnitude, abs(disVector.y) < Float.leastNonzeroMagnitude, abs(disVector.z) < Float.leastNonzeroMagnitude {
         return 0
     }
     
@@ -39,7 +39,7 @@ func positionOnLine(from point:simd_float3, to line:Line) -> simd_float3 {
 
 func isPointOnLine(point:simd_float3, line:Line) -> Bool {
     let vector = point - line.position
-    if abs(vector.x) < Float.leastNonzeroMagnitude, abs(vector.y) < Float.leastNonzeroMagnitude  {
+    if abs(vector.x) < Float.leastNonzeroMagnitude, abs(vector.y) < Float.leastNonzeroMagnitude, abs(vector.z) < Float.leastNonzeroMagnitude  {
         return true
     }
     let normalizedVector = normalize(vector)
@@ -47,16 +47,30 @@ func isPointOnLine(point:simd_float3, line:Line) -> Bool {
     let dotValue = dot(normalizedVector, normalizedDirection)
     return abs(dotValue - 1) < Float.leastNonzeroMagnitude
 }
+func isPointOnLine2(point:simd_float3, line:Line) -> Bool {
+    let vector = point - line.position
+    // 这一步判断可以省略
+    if abs(vector.x) < Float.leastNonzeroMagnitude, abs(vector.y) < Float.leastNonzeroMagnitude,abs(vector.z) < Float.leastNonzeroMagnitude  {
+        return true
+    }
+    let crossValue = cross(vector, line.direction)
+    return abs(crossValue.x) < Float.leastNonzeroMagnitude && abs(crossValue.y) < Float.leastNonzeroMagnitude && abs(crossValue.z) < Float.leastNonzeroMagnitude
+}
+func isPointOnLine3(point:simd_float3, line:Line) -> Bool {
+    let vector = point - line.position
+    let crossValue = cross(vector, line.direction)
+    return length_squared(crossValue) < Float.leastNonzeroMagnitude
+}
 
 func pointToLineTest() {
     let line = Line(position: simd_float3(1, 1, 1), direction: simd_float3(1, 1, 1))
-    let pointC = simd_float3(2, 4, 2)
+    let pointC = simd_float3(20000000000, 20000000000, 20000000000)
 
     let distance = distanceToLine(from: pointC, to: line)
     let tarPoint = positionOnLine(from: pointC, to: line)
 
     print("距离\(distance), 投影点\(tarPoint)")
-    print(isPointOnLine(point: pointC, line: line))
+    print(isPointOnLine2(point: pointC, line: line))
     if (abs(distance) < Float.leastNonzeroMagnitude) {
         print("点在直线上")
     } else {
