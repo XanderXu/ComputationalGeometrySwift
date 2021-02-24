e69e5165a8e02ad354afae3493319d0702ab5edb
XanderXu <517807635@qq.com>
2021-02-24 21:55:16 +0800

    矩阵爬行校正；

--- ComputationalGeometry/ComputationalGeometry/main.swift
+++ ComputationalGeometry/ComputationalGeometry/main.swift
@@ -8,5 +8,11 @@
 import Foundation
 import simd
 
-Line.pointToLineTest3()
+//Line.pointToLineTest3()
 //Plane.pointToPlaneTest3()
+let m = simd_float4x4([simd_float4([0.9, 0, 0.3, 0]),
+                        simd_float4([0, 0.9, 0.3, 0]),
+                        simd_float4([0.3, 0, 0.9, 0]),
+                        simd_float4([1, 1, 1, 1])
+                        ])
+print(m.orthogonalization())
