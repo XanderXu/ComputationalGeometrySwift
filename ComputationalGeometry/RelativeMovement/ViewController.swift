61a12545f27fd6159778f6c5c2992fe29198dd65
XanderXu <517807635@qq.com>
2021-03-28 21:28:28 +0800

    相对运动

---
+++ ComputationalGeometry/RelativeMovement/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  RelativeMovement
+//
+//  Created by CoderXu on 2021/3/28.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    var box1:SCNNode?
+    var box2:SCNNode?
+    var ship:SCNNode?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        box1 = scene.rootNode.childNode(withName: "box1", recursively: true)!
+        box2 = scene.rootNode.childNode(withName: "box2", recursively: true)!
+        ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        if let phoneT = sceneView.pointOfView?.simdTransform, let box1 = box1, let box2 = box2 {
+            ship?.simdTransform = box2.simdTransform * box1.simdTransform.inverse * phoneT
+        }
+    }
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
+
