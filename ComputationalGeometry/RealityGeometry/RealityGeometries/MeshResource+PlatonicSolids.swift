bb03a4b7bebb1c104cf472c44b8bb51906660d4f
许 <517807635@qq.com>
2022-04-06 18:10:26 +0800

    fix

--- ComputationalGeometry/RealityGeometry/RealityGeometries/MeshResource+PlatonicSolids.swift
+++ ComputationalGeometry/RealityGeometry/RealityGeometries/MeshResource+PlatonicSolids.swift
@@ -126,23 +126,139 @@ extension MeshResource {
         return try MeshResource.generate(from: [descr])
     }
     public static func generateHexahedron(radius: Float, res: Int) throws -> MeshResource {
+        let pointCount = 8
+        var quads = 6
+        var vertices = pointCount * 3
+        
         var descr = MeshDescriptor()
         var meshPositions: [SIMD3<Float>] = []
         var indices: [UInt32] = []
-        var normals: [SIMD3<Float>] = []
+        var normals: [SIMD3<Float>] = Array(repeating: .zero, count: vertices)
         var textureMap: [SIMD2<Float>] = []
-        var materials: [UInt32] = []
         
+        let a: Float = 2 * radius * sqrtf(3)//棱长
+        let r = a / 2 //内切球半径
+        let points: [SIMD3<Float>] = [
+            SIMD3<Float>(r, r, r),
+            SIMD3<Float>(-r, r, r),
+            SIMD3<Float>(-r, r, -r),
+            SIMD3<Float>(r, r, -r),
+            
+            SIMD3<Float>(r, -r, r),
+            SIMD3<Float>(-r, -r, r),
+            SIMD3<Float>(-r, -r, -r),
+            SIMD3<Float>(r, -r, -r),
+        ]
+        meshPositions.append(contentsOf: points + points + points)
         
+        let index: [UInt32] = [
+            0, 1, 2, 3,
+            4, 5, 6, 7,
+            
+            7, 4, 0, 3,
+            0, 1, 3,
+            
+            2, 3, 1
+        ]
+        var countDict: [UInt32:Int] = [:]
+        for ind in index {
+            let count = countDict[ind] ?? 0
+            indices.append(ind + UInt32(pointCount * count))
+            countDict[ind] = count + 1
+        }
         
+        for i in 0..<quads {
+            let ai = 3 * i
+            let bi = 3 * i + 1
+            let ci = 3 * i + 2
+            let di = 3 * i + 3
+            
+            let i0 = indices[ai]
+            let i1 = indices[bi]
+            let i2 = indices[ci]
+            let i3 = indices[di]
+            
+            let v0 = meshPositions[Int(i0)]
+            let v1 = meshPositions[Int(i1)]
+            let v2 = meshPositions[Int(i2)]
+            let v3 = meshPositions[Int(i3)]
+            
+            let faceNormal = simd_normalize((v0 + v1 + v2 + v3) / 4)
+            normals[Int(i0)] = faceNormal
+            normals[Int(i1)] = faceNormal
+            normals[Int(i2)] = faceNormal
+            normals[Int(i3)] = faceNormal
+        }
         
+        for _ in 0..<res {
+            let newQuads = quads * 4
+            let newVertices = vertices + quads * 5
+            
+            var newIndices: [UInt32] = []
+            var pos: SIMD3<Float>
+            
+            for i in 0..<quads {
+                let ai = 3 * i
+                let bi = 3 * i + 1
+                let ci = 3 * i + 2
+                let di = 3 * i + 3
+                
+                let i0 = indices[ai]
+                let i1 = indices[bi]
+                let i2 = indices[ci]
+                let i3 = indices[di]
+                
+                let v0 = meshPositions[Int(i0)]
+                let v1 = meshPositions[Int(i1)]
+                let v2 = meshPositions[Int(i2)]
+                let v3 = meshPositions[Int(i3)]
+                
+                let faceNormal = normals[Int(i0)]
+                normals.append(faceNormal)
+                
+                pos = (v0 + v1 + v2 + v3) / 4
+                meshPositions.append(pos)
+                
+                pos = (v0 + v1) / 2
+                meshPositions.append(pos)
+                
+                pos = (v1 + v2) / 2
+                meshPositions.append(pos)
+                
+                pos = (v2 + v3) / 2
+                meshPositions.append(pos)
+                
+                pos = (v0 + v3) / 2
+                meshPositions.append(pos)
+
+                
+                let a = UInt32(ai + vertices)
+                let b = UInt32(bi + vertices)
+                let c = UInt32(ci + vertices)
+                let d = UInt32(di + vertices)
+                newIndices.append(contentsOf: [
+                    i0, a, c,
+                    a, i1, b,
+                    a, b, c,
+                    c, b, i2
+                ])
+            }
+            
+            indices = newIndices
+            quads = newQuads
+            vertices = newVertices
+        }
+        
+        for i in 0..<meshPositions.count {
+            let p = meshPositions[i]
+            let n = p//simd_normalize(p)
+          
+            textureMap.append(SIMD2<Float>(abs(atan2(n.x, n.z)) / .pi, 1 - acos(n.y/radius) / .pi))
+        }
         descr.positions = MeshBuffers.Positions(meshPositions)
         descr.normals = MeshBuffers.Normals(normals)
         descr.textureCoordinates = MeshBuffers.TextureCoordinates(textureMap)
-        descr.primitives = .triangles(indices)
-        if !materials.isEmpty {
-            descr.materials = MeshDescriptor.Materials.perFace(materials)
-        }
+        descr.primitives = .trianglesAndQuads(triangles: [], quads: indices)
         return try MeshResource.generate(from: [descr])
     }
     public static func generateOctahedron(radius: Float, res: Int) throws -> MeshResource {
