0c7dfd6c18e5ba295e94879d7c4285344ec9d082
许 <517807635@qq.com>
2022-04-07 15:43:31 +0800

    fix

--- ComputationalGeometry/RealityGeometry/RealityGeometries/MeshResource+PlatonicSolids.swift
+++ ComputationalGeometry/RealityGeometry/RealityGeometries/MeshResource+PlatonicSolids.swift
@@ -390,23 +390,179 @@ extension MeshResource {
         return try MeshResource.generate(from: [descr])
     }
     /// 正十二面体，radius 为外接球半径，res 五边形面剖分次数
-    public static func generateDogecahedron(radius: Float, res: Int) throws -> MeshResource {
+    public static func generateDogecahedron(radius: Float, res: Int = 0) throws -> MeshResource {
+        let pointCount = 20
+        let pentagons = 12
+        var vertices = pointCount * 3
+        
         var descr = MeshDescriptor()
         var meshPositions: [SIMD3<Float>] = []
         var indices: [UInt32] = []
-        var normals: [SIMD3<Float>] = []
+        var normals: [SIMD3<Float>] = Array(repeating: .zero, count: vertices)
         var textureMap: [SIMD2<Float>] = []
-        var materials: [UInt32] = []
         
+        let phi = (1.0 + sqrtf(5)) * 0.5
+        let a = (sqrtf(5) - 1) / sqrtf(3) * radius //棱长
+        let h =  phi / sqrtf(3) * radius //中交球半径
+        let w = a / 2
+        let v = radius / sqrtf(3)
+
+        let points = [
+            SIMD3<Float>(0.0, h, w),
+            SIMD3<Float>(0.0, h, -w),
+            SIMD3<Float>(0.0, -h, w),
+            SIMD3<Float>(0.0, -h, -w),
+
+            SIMD3<Float>(h, -w, 0.0),
+            SIMD3<Float>(h, w, 0.0),
+            SIMD3<Float>(-h, -w, 0.0),
+            SIMD3<Float>(-h, w, 0.0),
+
+            SIMD3<Float>(-w, 0.0, -h),
+            SIMD3<Float>(w, 0.0, -h),
+            SIMD3<Float>(-w, 0.0, h),
+            SIMD3<Float>(w, 0.0, h),
+            
+            SIMD3<Float>(v, v, v),
+            SIMD3<Float>(-v, v, v),
+            SIMD3<Float>(-v, v, -v),
+            SIMD3<Float>(v, v, -v),
+            
+            SIMD3<Float>(v, -v, v),
+            SIMD3<Float>(-v, -v, v),
+            SIMD3<Float>(-v, -v, -v),
+            SIMD3<Float>(v, -v, -v),
+        ]
+        meshPositions.append(contentsOf: points + points + points)
         
+        let index: [UInt32] = [
+            0, 11, 5,
+            0, 5, 1,
+            0, 1, 7,
+            0, 7, 10,
+            0, 10, 11,
+
+            1, 5, 9,
+            5, 11, 4,
+            11, 10, 2,
+            10, 7, 6,
+            7, 1, 8,
+
+            3, 9, 4,
+            3, 4, 2,
+            3, 2, 6,
+            3, 6, 8,
+            3, 8, 9,
+
+            4, 9, 5,
+            2, 4, 11,
+            6, 2, 10,
+            8, 6, 7,
+            9, 8, 1
+        ]
+        var countDict: [UInt32:Int] = [:]
+        for ind in index {
+            let count = countDict[ind] ?? 0
+            indices.append(ind + UInt32(pointCount * count))
+            countDict[ind] = count + 1
+        }
         
+        for i in 0..<pentagons {
+            let ai = 5 * i
+            let bi = 5 * i + 1
+            let ci = 5 * i + 2
+            let di = 5 * i + 3
+            let ei = 5 * i + 5
+            
+            let i0 = indices[ai]
+            let i1 = indices[bi]
+            let i2 = indices[ci]
+            let i3 = indices[di]
+            let i4 = indices[ei]
+            
+            let v0 = meshPositions[Int(i0)]
+            let v1 = meshPositions[Int(i1)]
+            let v2 = meshPositions[Int(i2)]
+            let v3 = meshPositions[Int(i3)]
+            let v4 = meshPositions[Int(i4)]
+            
+            let faceNormal = simd_normalize((v0 + v1 + v2 + v3 + v4) / 5)
+            normals[Int(i0)] = faceNormal
+            normals[Int(i1)] = faceNormal
+            normals[Int(i2)] = faceNormal
+            normals[Int(i3)] = faceNormal
+            normals[Int(i4)] = faceNormal
+        }
+        
+        var triangles = pentagons * 5
+        for j in 1..<res {
+            let newTriangles = triangles * 4
+            let newVertices = vertices + triangles * 3
+            
+            var newIndices: [UInt32] = []
+            var pos: SIMD3<Float>
+            if j == 1 {
+                
+            } else {
+                for i in 0..<triangles {
+                    let ai = 3 * i
+                    let bi = 3 * i + 1
+                    let ci = 3 * i + 2
+                    
+                    let i0 = indices[ai]
+                    let i1 = indices[bi]
+                    let i2 = indices[ci]
+                    
+                    let v0 = meshPositions[Int(i0)]
+                    let v1 = meshPositions[Int(i1)]
+                    let v2 = meshPositions[Int(i2)]
+                    
+                    let faceNormal = normals[Int(i0)]
+                    normals.append(contentsOf: [faceNormal, faceNormal, faceNormal])
+                    // a
+                    pos = (v0 + v1) * 0.5
+                    meshPositions.append(pos)
+                    
+                    // b
+                    pos = (v1 + v2) * 0.5
+                    meshPositions.append(pos)
+                    
+                    // c
+                    pos = (v2 + v0) * 0.5
+                    meshPositions.append(pos)
+                    
+                    
+                    let a = UInt32(ai + vertices)
+                    let b = UInt32(bi + vertices)
+                    let c = UInt32(ci + vertices)
+                    newIndices.append(contentsOf: [
+                        i0, a, c,
+                        a, i1, b,
+                        a, b, c,
+                        c, b, i2
+                    ])
+                }
+            }
+            
+            indices = newIndices
+            triangles = newTriangles
+            vertices = newVertices
+        }
+        
+        for i in 0..<meshPositions.count {
+            let p = meshPositions[i]
+            let n = p//simd_normalize(p)
+          
+            textureMap.append(SIMD2<Float>(abs(atan2(n.x, n.z)) / .pi, 1 - acos(n.y/radius) / .pi))
+        }
         
         descr.positions = MeshBuffers.Positions(meshPositions)
         descr.normals = MeshBuffers.Normals(normals)
         descr.textureCoordinates = MeshBuffers.TextureCoordinates(textureMap)
-        descr.primitives = .triangles(indices)
-        if !materials.isEmpty {
-            descr.materials = MeshDescriptor.Materials.perFace(materials)
+        if res == 0 {
+            descr.primitives = .polygons(Array(repeating: 5, count: indices.count), indices)
+        } else {
+            descr.primitives = .triangles(indices)
         }
         return try MeshResource.generate(from: [descr])
     }
