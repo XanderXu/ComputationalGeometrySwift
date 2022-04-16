d96d83697d06a00458172c8fa2e66f1fa078d994
Xander Xu <xander-xu@foxmail.com>
2022-04-16 16:34:58 +0800

    fix

--- ComputationalGeometry/RealityGeometry/RealityGeometries/MeshResource+Cube.swift
+++ ComputationalGeometry/RealityGeometry/RealityGeometries/MeshResource+Cube.swift
@@ -8,7 +8,7 @@
 import RealityKit
 
 extension MeshResource {
-    public static func generateExtrudedRoundedRectPad(width: Float, height: Float, depth: Float, radius: Float, angularResolution: Int = 24, edgeXResolution: Int = 2, edgeYResolution: Int = 2, depthResolution: Int = 1, radialResolution: Int = 2, splitFaces: Bool = false, circleUV: Bool = false) throws -> MeshResource {
+    public static func generateExtrudedRoundedRectPad(width: Float, height: Float, depth: Float, radius: Float, angularResolution: Int = 3, edgeXResolution: Int = 2, edgeYResolution: Int = 2, depthResolution: Int = 2, radialResolution: Int = 2, splitFaces: Bool = false, circleUV: Bool = false) throws -> MeshResource {
         var descr = MeshDescriptor()
         var meshPositions: [SIMD3<Float>] = []
         var indices: [UInt32] = []
@@ -19,6 +19,7 @@ extension MeshResource {
         let halfDepth = depth * 0.5
         
         let datas = generateRoundedRectPlaneDatas(width: width, height: height, radius: radius, angularResolution: angularResolution, edgeXResolution: edgeXResolution, edgeYResolution: edgeYResolution, radialResolution: radialResolution, circleUV: circleUV)
+        let planePositionsCount = datas.meshPositions.count
         
         let topMeshPositions = datas.meshPositions.map({ p in
             return p + SIMD3<Float>(0, halfDepth, 0)
@@ -29,8 +30,8 @@ extension MeshResource {
         })
         meshPositions.append(contentsOf: bottomMeshPositions)
         
-        normals.append(contentsOf: Array(repeating: SIMD3<Float>(0, 1, 0), count: datas.meshPositions.count))
-        normals.append(contentsOf: Array(repeating: SIMD3<Float>(0, -1, 0), count: datas.meshPositions.count))
+        normals.append(contentsOf: Array(repeating: SIMD3<Float>(0, 1, 0), count: planePositionsCount))
+        normals.append(contentsOf: Array(repeating: SIMD3<Float>(0, -1, 0), count: planePositionsCount))
         
         textureMap.append(contentsOf: datas.textureMap)
         textureMap.append(contentsOf: datas.textureMap.map({ uv in
@@ -41,7 +42,10 @@ extension MeshResource {
         var reverseIndices: [UInt32] = []
         let bottomTriangleCount = datas.indices.count / 3
         for i in 1...bottomTriangleCount {
-            reverseIndices.append(contentsOf: [datas.indices[i*3-1], datas.indices[i*3-2], datas.indices[i*3-3]])
+            reverseIndices.append(contentsOf: [
+                datas.indices[i*3-1] + UInt32(planePositionsCount),
+                datas.indices[i*3-2] + UInt32(planePositionsCount),
+                datas.indices[i*3-3] + UInt32(planePositionsCount)])
         }
         indices.append(contentsOf: reverseIndices)
         
@@ -52,60 +56,79 @@ extension MeshResource {
         let angular = angularResolution > 2 ? angularResolution : 3
         let edgeX = edgeXResolution > 1 ? edgeXResolution : 2
         let edgeY = edgeYResolution > 1 ? edgeYResolution : 2
-        let edgeDepth = depthResolution > 0 ? depthResolution : 1
+        let edgeDepth = depthResolution > 1 ? depthResolution : 2
         
         let widthHalf = width * 0.5
         let heightHalf = height * 0.5
         let minDim = (widthHalf < heightHalf ? widthHalf : heightHalf)
         let radius = radius > minDim ? minDim : radius
+        let innerWidth = width - radius * 2
+        let innerHeight = height - radius * 2
         
         let perLoop = (angular - 2) * 4 + (edgeX * 2) + (edgeY * 2) + (circleUV ? 2 : 0)
-        let perimeter = width * 2 + height * 2 + .pi * radius * 2
+        let perimeter = innerWidth * 2 + innerHeight * 2 + .pi * radius * 2
+        let bottomOutPositions = Array(bottomMeshPositions[(planePositionsCount - perLoop)...])
         
-        let bottomOutPositions = bottomMeshPositions[(bottomMeshPositions.count - perLoop)...]
-        let depthInc = depth / Float(edgeDepth)
-        let XInc = (width - 2 * radius) / Float(edgeY)
-        let YInc = (height - 2 * radius) / Float(edgeX)
-        let arcInc = .pi * radius * 0.5 / Float(angular)
+        let depthInc = depth / Float(edgeDepth-1)
+        let angularInc = .pi * radius * 0.5 / Float(angular - 1)
+        let innerWidthHalf = widthHalf - radius
+        let innerHeightHalf = heightHalf - radius
         
-        for j in 0...edgeDepth {
+        
+        let index1 = edgeY + (circleUV ? 2 : 0)
+        let keyIndexes = [0,
+                          index1 - 1,
+                          index1 + angular - 2,
+                          index1 + angular + edgeX - 3,
+                          index1 + angular * 2 + edgeX - 4,
+                          index1 + angular * 2 + edgeX + edgeY - 5,
+                          index1 + angular * 3 + edgeX + edgeY - 6,
+                          index1 + angular * 3 + edgeX * 2 + edgeY - 7
+        ]
+        let keyLengths = [0,
+                          innerHeight,
+                          innerHeight + .pi * radius * 0.5,
+                          innerHeight + innerWidth + .pi * radius * 0.5,
+                          innerHeight + innerWidth + .pi * radius,
+                          innerHeight * 2 + innerWidth + .pi * radius,
+                          innerHeight * 2 + innerWidth + .pi * radius * 1.5,
+                          innerHeight * 2 + innerWidth * 2 + .pi * radius * 1.5,
+        ]
+        let topBottomPositionsCount = UInt32(planePositionsCount * 2)
+        
+        for j in 0..<edgeDepth {
             let jf = Float(j)
             let d = jf * depthInc
-            let uvy = jf / Float(edgeDepth)
+            let uvy = jf / Float(edgeDepth-1)
             let curLoop = j * perLoop
             let nextLoop = (j + 1) * perLoop
             
-            var length: Float = 0
             for i in 0..<perLoop {
                 let p = bottomOutPositions[i]
                 meshPositions.append(p + SIMD3<Float>(0, d, 0))
-                var inner = p
-                if inner.x > widthHalf {
-                    inner.x = widthHalf
-                }
-                if inner.x < -widthHalf {
-                    inner.x = -widthHalf
-                }
-                if inner.z > heightHalf {
-                    inner.z = heightHalf
-                }
-                if inner.z < -heightHalf {
-                    inner.z = -heightHalf
-                }
+                
+                let inner = p.clamped(lowerBound: SIMD3<Float>(-innerWidthHalf, 0, -innerHeightHalf), upperBound: SIMD3<Float>(innerWidthHalf, 0, innerHeightHalf))
                 let n = simd_normalize(p - inner)
                 normals.append(n)
                 
-                let line1 = edgeY + (circleUV ? 2 : 0)
-                if i <= line1 {
-                    length += YInc
-                } else if i <= line1 + angular - 2 {
-                    length += arcInc
-                } else if i < line1 + angular - 2 + edgeX {
-                    
+                var length: Float = 0
+                if i <= keyIndexes[1] {
+                    length += keyLengths[0] + abs(p.z - bottomOutPositions[0].z)
+                } else if i <= keyIndexes[2] {
+                    length += keyLengths[1] + angularInc * Float(i - keyIndexes[1])
+                } else if i <= keyIndexes[3] {
+                    length += keyLengths[2] + abs(p.x - bottomOutPositions[2].x)
+                } else if i <= keyIndexes[4] {
+                    length += keyLengths[3] + angularInc * Float(i - keyIndexes[3])
+                } else if i <= keyIndexes[5] {
+                    length += keyLengths[4] + abs(p.z - bottomOutPositions[4].z)
+                } else if i <= keyIndexes[6] {
+                    length += keyLengths[5] + angularInc * Float(i - keyIndexes[5])
+                } else if i <= keyIndexes[7] {
+                    length += keyLengths[6] + abs(p.x - bottomOutPositions[6].x)
+                } else {
+                    length += keyLengths[7] + angularInc * Float(i - keyIndexes[7])
                 }
-                
-                
-                
                 textureMap.append(SIMD2<Float>(length / perimeter, uvy))
                 
                 var prev = i - 1
@@ -113,12 +136,12 @@ extension MeshResource {
                 let curr = i
                 let next = (i + 1) % perLoop
                 
-                if j != edgeDepth {
-                    let i0 = UInt32(curLoop + curr)
-                    let i1 = UInt32(curLoop + next)
+                if j != edgeDepth - 1 {
+                    let i0 = UInt32(curLoop + curr) + topBottomPositionsCount
+                    let i1 = UInt32(curLoop + next) + topBottomPositionsCount
                     
-                    let i2 = UInt32(nextLoop + curr)
-                    let i3 = UInt32(nextLoop + next)
+                    let i2 = UInt32(nextLoop + curr) + topBottomPositionsCount
+                    let i3 = UInt32(nextLoop + next) + topBottomPositionsCount
                     indices.append(contentsOf: [
                         i0, i2, i3,
                         i0, i3, i1
